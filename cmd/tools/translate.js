189b17cdadedb04f46b82c9d462841c5d3bbc7e2
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2024-12-11 21:04:37 +0700

    update cmd/tools/translate.js

--- cmd/tools/translate.js
+++ cmd/tools/translate.js
@@ -6,7 +6,7 @@ export default (handler) => {
         run: async (m, { func }) => {
             if (m.quoted) {
                 if (!m.text) return m.reply('Silahkan masukan kode bahasa\ncontoh: .tr en', true)
-                m.reply(func.translate(m.quoted.body, m.text).text())
+                m.reply(func.translate(m.quoted.body, m.text).toString())
             } else {
                 const last = m.text.lastIndexOf(",")
                 const txt = m.text.slice(0, last).trim()
@@ -14,7 +14,7 @@ export default (handler) => {
 
                 if (!txt) return m.reply('Silahkan masukan teks\ncontoh: .tr selamat pagi,en', true)
                 if (!code) return m.reply('Silahkan masukan kode bahasa\ncontoh: .tr selamat pagi,en', true)
-                m.reply(func.translate(txt, code).text())
+                m.reply(func.translate(txt, code).toString())
             }
         }
     })
