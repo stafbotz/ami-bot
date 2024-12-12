f06ed95b98971080b088a778230fb00ff8793269
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2024-12-12 10:52:46 +0700

    update system/db/schema.js

--- system/db/schema.js
+++ system/db/schema.js
@@ -15,7 +15,6 @@ const schema = async (m, sock, db) => {
         if (!isNumber(user.afk)) user.afk = -1
         if (!("afk_reason" in user)) user.afk_reason = ""
         if (!isNumber(user.exp)) user.exp = 0
-        /*if (!isNumber(user.limit)) user.limit = 10*/
         if (!isNumber(user.saldo)) user.saldo = 0
         if (!isNumber(user.point)) user.point = 0
         if (!("exp_prem" in user)) user.exp_prem = 0
