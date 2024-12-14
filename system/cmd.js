2b9be221ee9735dbd5d433685c563475a4b0d6ee
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2024-12-14 22:18:27 +0700

    update system/cmd.js

--- system/cmd.js
+++ system/cmd.js
@@ -195,9 +195,7 @@ export default class CommandHandler {
         if (!usr.progressreg) {
             await sock.sendMessage(
                 m.from,
-                {
-                    text: "Hai! 👋 Aku Ami Bot, kita kenalan dulu yuk, biar lebih akrab. Nama kamu siapa, ya? ☺️"
-                },
+                { text: "Hai! 👋 Aku Ami Bot, kita kenalan dulu yuk, biar lebih akrab. Nama kamu siapa, ya? ☺️" },
                 { quoted: m, ephemeralExpiration: m.expiration }
             );
         }
