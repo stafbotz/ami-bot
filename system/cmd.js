d37cf4161ead1e2b929de4c68229092a3635e3ff
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-01-17 11:48:06 +0700

    update system/cmd.js

--- system/cmd.js
+++ system/cmd.js
@@ -175,7 +175,6 @@ export default class CommandHandler {
                         { quoted: m, ephemeralExpiration: m.expiration }
                     );
                 }
-                usr.progressreg = 1;
                 return await this.handleRegister(
                     usr,
                     sock,
