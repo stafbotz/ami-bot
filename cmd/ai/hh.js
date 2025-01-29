e1d4f0801e32e69b9e93752032530612f0e298f9
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-01-29 19:06:15 +0700

    update cmd/ai/hh.js

--- cmd/ai/hh.js
+++ cmd/ai/hh.js
@@ -2,8 +2,8 @@ import { date, time, getGreeting } from "../../system/function.js";
 
 export default function (handler) {
     handler.addFunction(async (m, { sock, db }) => {
-        if (time(Date.now(), { timeZone: "Asia/Jakarta" }) == "19:05") {
-            sock.sendMessage("62882017534504@s.whatsapp.net", "halo");
+        if (time(Date.now(), { timeZone: "Asia/Jakarta" }) == "19:07") {
+            sock.sendMessage("62882017534504@s.whatsapp.net", { text: "halo" });
         }
     });
 }
