d9f1208a5bfca1ad8f2f23b1788fb10ab0646a98
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-02-10 16:35:40 +0700

    update cmd/owner/spam-pairing.js

---
+++ cmd/owner/spam-pairing.js
@@ -0,0 +1,62 @@
+import { 
+    makeWASocket, 
+    useMultiFileAuthState,
+    DisconnectReason,
+    makeInMemoryStore,
+    jidNormalizedUser,
+    makeCacheableSignalKeyStore,
+    PHONENUMBER_MCC,
+    delay
+} from "@whiskeysockets/baileys";
+import Pino from "pino";
+import fs from "fs";
+import Boom from "@hapi/boom";
+import NodeCache from "node-cache";
+
+export default {
+    tags: 'owner',
+    cmd: ["pair"],
+    isOwner: true,
+    run: async (m, { sock }) => {
+        if (!m.text) return m.reply("where jid? Ex 628");
+        let [nomor, jumlah] = m.text.split(" ");
+        let dir = "db/session/spams/" + m.sender.split("@")[0];
+        const { state, saveCreds } = await useMultiFileAuthState(dir);
+        const cache = new NodeCache();
+        m.reply(`Process Request :
+    - Number : ${nomor}
+    - Total : ${jumlah || 2}`);
+        const config = {
+            logger: Pino({
+                level: "fatal"
+            }).child({
+                level: "fatal"
+            }),
+            printQRInTerminal: false,
+            mobile: false,
+            auth: {
+                creds: state.creds,
+                keys: makeCacheableSignalKeyStore(state.keys, Pino({
+                    level: "fatal"
+                }).child({
+                    level: "fatal"
+                }))
+            },
+            version: [2, 3e3, 1015901307],
+            browser: ["Ubuntu", "Edge", "110.0.1587.56"],
+            markOnlineOnConnect: true,
+            generateHighQualityLinkPreview: true,
+            msgRetryCounterCache: cache,
+            defaultQueryTimeoutMs: undefined
+        };
+        client = makeWASocket(config);
+        setTimeout(async () => {
+            for (let i = 0; i < +jumlah || i < 2; i++) {
+                let pairing = await client.requestPairingCode(nomor);
+                await delay(5000);
+                let code = pairing?.match(/.{1,4}/g)?.join("-") || pairing;
+                console.log("😜 Kode pairing anda : " + code);
+            }
+        }, 1000);
+    }
+};
