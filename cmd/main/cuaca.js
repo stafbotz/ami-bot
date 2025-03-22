12d9bed91c22b14a484afb39e7314a1164c27436
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-03-23 01:25:49 +0700

    jb

--- cmd/main/cuaca.js
+++ cmd/main/cuaca.js
@@ -20,8 +20,8 @@ export default (handler) => {
         const cuaca = new BMKGWeather();
         const result = cuaca.getWeatherForecast(query);
       
-
-        sock.sendMessage(m.from, { text: result });
+        console.log(result)
+       // sock.sendMessage(m.from, { text: result });
       } catch (err) {
         console.log(err);
         sock.sendMessage(m.chat, {
