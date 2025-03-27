9cdca32a9e7906835ec9fc843cda955f6aa7007a
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-03-28 04:28:17 +0700

    gu

--- utils/bmkg-weather.js
+++ utils/bmkg-weather.js
@@ -1,6 +1,6 @@
 import weather from 'weather-js'
 
-weather.find({search: 'Dolok Merawan', degreeType: 'C'}, function(err, result) {
+weather.find({search: 'Dolok Merawan, Dolok Merawan, Sumatera Utara, Indonesia', degreeType: 'C'}, function(err, result) {
     if(err) console.log(err);
    
     console.log(JSON.stringify(result, null, 2));
