7f8e76b7a3815969924a0bbdf86ce4806196a954
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-03-28 04:25:06 +0700

    ugu

--- utils/bmkg-weather.js
+++ utils/bmkg-weather.js
@@ -1,6 +1,6 @@
 import weather from 'weather-js'
 
-weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+weather.find({search: '20993', degreeType: 'C'}, function(err, result) {
     if(err) console.log(err);
    
     console.log(JSON.stringify(result, null, 2));
