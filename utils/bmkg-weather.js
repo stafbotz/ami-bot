343f535a5a07fe3746e85ae037413ef2e7013631
Nadia Almira <90103953+stafbotz@users.noreply.github.com>
2025-03-28 04:26:00 +0700

    ugu

--- utils/bmkg-weather.js
+++ utils/bmkg-weather.js
@@ -1,6 +1,6 @@
 import weather from 'weather-js'
 
-weather.find({search: '20993', degreeType: 'C'}, function(err, result) {
+weather.find({search: 'Dolok Merawan', degreeType: 'C'}, function(err, result) {
     if(err) console.log(err);
    
     console.log(JSON.stringify(result, null, 2));
